17a586920c3406375d99a0a858da5847a398947a
Eka0811 <ekanshgupta916@gmail.com>
2026-08-28 18:25:02 +0530

    Add files via upload

---
+++ 100D.19.1.c
@@ -0,0 +1,15 @@
+//Write a program to find the LCM of two numbers.
+#include <stdio.h>
+int main()
+{
+    int a, b, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    lcm = (a > b) ? a : b;
+    while (lcm % a != 0 || lcm % b != 0) 
+    {
+        lcm++;
+    }
+    printf("LCM of %d and %d = %d\n", a, b, lcm);
+    return 0;
+}
